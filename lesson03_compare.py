04e5cdd7b230b28fbe6e90237ae4f0b8a3664741
apillalamarri <anupama.pillalamarri@gmail.com>
2014-04-15 20:29:51 -0400

    Update and rename compare.py to lesson03_compare.py
    This has some code in it that's useless right now because I misunderstood the question, but I wanted to keep it for future reference. The dict_to_csv function turns a dictionary into a csv but assumes the dictionary is ordered, which it's not.
    
    This code will add anyone who is not found in survey.csv to a csv called "extra_employees.csv" whereas the prompt was to add the extra information about employees to their own rows in all_employees.csv.

--- lesson03_compare.py
+++ lesson03_compare.py
@@ -34,20 +34,57 @@ def csv_to_dict(csv_file, k):
 			single_line_dict[header] = element
 			dict[single_line_dict.get(k)] = single_line_dict
 	return dict
-
+"""
+def dict_to_csv (dict):
+	"This is probably useless. It takes a dictionary and turns it into a csv but it assumes that the dictionary is ordered which of course it isn't."
+	csv_string = ""
+	for v in dict.itervalues():
+		header = ""
+		for sk in v.iterkeys():
+			header += sk
+			header += ","
+		header = header[:-1]+"\n"
+		for sv in v.itervalues():
+			csv_string+=(sv+',')
+		csv_string+=('\n')
+	csv_string = csv_string.replace(",\n","\n")
+	csv_string = header + csv_string
+	return csv_string
+"""
 with open ("all_employees.csv", "r") as all_employees_file:
 	emp_dict = csv_to_dict(all_employees_file, "name")
+print "emp_dict is now:"
+print emp_dict	
+
+emp_csv = dict_to_csv(emp_dict)
+print "emp_csv is now:"
+print emp_csv
+
 with open ("survey.csv", "r") as survey_file:
 	survey_dict = csv_to_dict(survey_file, "email")
-	
-for email, info in survey_dict.iteritems():
-	for name, emp in emp_dict.iteritems():
-		if email == emp.get('email'):
-			print "{0} took the survey! Here is her contact information: ".format(name)
-			for k, v in emp.iteritems():
-				print "{0}: {1}".format(k, v)
-			print "\n"
+
+with open ("extra_employees.csv", "w") as extra_emp:
+	for email, info in survey_dict.iteritems():
+		for name, emp in emp_dict.iteritems():
+			if email == emp.get('email'):
+				print "{0} took the survey! Here is her contact information: ".format(name)
+				for k, v in emp.iteritems():
+					#print "{0}: {1}".format(k, v)
+					print "\n"
+			else:
+				#print "No",
+				#print emp
+				extra_emp.write(emp.get("department")+",")
+				extra_emp.write(emp.get("phone")+",")
+				extra_emp.write(emp.get("position")+",")
+				extra_emp.write(emp.get("name")+",")
+				extra_emp.write(emp.get("email")+"\n")
+
 			
 # Challenge 2: Add the extra information from survey.csv into all_employees.csv as extra columns.  
 # IMPORTANT: It would probably be a good idea to save it as an extra file instead of accidentally overwriting your original!
 # By the end, your all_employees.csv should contain the following columns: name, email, phone, department, position, #twitter, github
+
+
+
+
